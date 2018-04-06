41ceb79f671ef30503861d0b6a46f6e78ca9bf62
Lucas Cazeto <lucas.cazeto@gmail.com>
2018-04-06 10:10:57 +0200

    using dev on local machine, adjusted table

--- db/migrations/002_create_list_table.rb
+++ db/migrations/002_create_list_table.rb
@@ -3,9 +3,15 @@ Sequel.migration do
           create_table :lists do 
                primary_key :id 
                String :name, :length => 32, :null => false 
-               column :shared_with, 'enum("private", "shared", "public")', :null => false, :default => 'private' 
+               column :shared_with, 'enum("private", "public")', :null => false, :default => 'private' 
                DateTime :created_at
                DateTime :updated_at 
           end 
      end 
 end 
+
+# Sequel.migration do
+#      change do
+#           alter_table(:lists) {add_column :public, TrueClass, :null => false, :default => false}
+#      end
+# end
