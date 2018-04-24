a4f11be8f7cd32703d97cf701fdce6c56b8e8310
Lucas Cazeto <lucas.cazeto@gmail.com>
2018-04-24 10:36:38 +0200

    minor

--- lib/application.rb
+++ lib/application.rb
@@ -2,6 +2,7 @@ class Todo < Sinatra::Application
   before do
     redirect '/login' if !%w[login signup].include?(request.path_info.split('/')[1]) && session[:user_id].nil?
 
+    # get user before every route if session exists.
     @user ||= User.first(id: session[:user_id]) if session[:user_id]
   end
 end
